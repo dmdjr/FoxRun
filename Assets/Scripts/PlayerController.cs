f3b9b211ed578b5a34591200ff46257cd2a5dcb9
Song <awesomeday322@gmail.com>
2025-07-23 15:18:28 +0900

    feat(player): character move & moving anim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,31 +7,57 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
     private float moveInput;
     private bool isGrounded;
 
     public LayerMask groundLayer;
     public float groundCheckDistance = 1.2f;
-    void Start()
+
+
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
     }
+
 
     void Update()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            spriteRenderer.flipX = true;
             moveInput = -1;
+            animator.SetBool("isRun", true);
+
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
+            spriteRenderer.flipX = false;
             moveInput = 1;
+            animator.SetBool("isRun", true);
+
         }
-        else moveInput = 0;
+        else
+        {
+            moveInput = 0;
+            animator.SetBool("isRun", false);
+        }
 
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
-        isGrounded = hit.collider != null;
         Debug.DrawRay(transform.position, Vector2.down * groundCheckDistance, Color.green);
+        if (hit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
@@ -42,7 +68,6 @@ public class PlayerController : MonoBehaviour
     {
         // 물리 프레임 기준으로 위치 이동
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
-
 
     }
 }
