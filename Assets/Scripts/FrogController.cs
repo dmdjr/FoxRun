56e58f9691fa615f50e536091c6d4a3a1faf738b
Song <awesomeday322@gmail.com>
2025-07-31 20:51:37 +0900

    feat(AI): Frog JumpAnimation

--- Assets/Scripts/FrogController.cs
+++ Assets/Scripts/FrogController.cs
@@ -12,19 +12,30 @@ public class FrogController : MonoBehaviour
     float thinkTime = 0f; // 랜덤으로 정해질 생각 시간
     int objActionNum = 0; // AI가 어떤 행동을 할 지 정해주는 숫자(1이면 xFlip, 2이면 해당 방향으로 점프)
 
+    private bool isGrounded;
+    public LayerMask groundLayer;
+    public float groundCheckDistance = 1.2f;
     SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
+    private Animator animator;
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         Think();
     }
-
+    private void Update()
+    {
+        CheckGround();
+        animator.SetBool("isJumping", !isGrounded);
+    }
     void Think()
     {
         thinkTime = Random.Range(minThinkTime, maxThinkTime);
         objActionNum = Random.Range(1, 11);
+
+
         if (objActionNum >= 1 && objActionNum < 8)
         {
             spriteRenderer.flipX = true;
@@ -37,5 +48,11 @@ public class FrogController : MonoBehaviour
         }
 
         Invoke("Think", thinkTime);
+    }
+    void CheckGround()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
+        Debug.DrawRay(transform.position, Vector2.down * groundCheckDistance, Color.green);
+        isGrounded = hit.collider != null;
     }
 }
